5950308bf2a5887a7d1128776e2925b9fa121469
박영현(Yeonghyeon Park)/IoT개발팀/SKP <young200405@gmail.com>
2024-09-20 20:13:58 +0900

    script modified

--- experiments/download_mvtecad.sh
+++ experiments/download_mvtecad.sh
@@ -3,5 +3,5 @@ cd mvtec
 # Download MVTec anomaly detection dataset
 wget https://www.mydrive.ch/shares/38536/3830184030e49fe74747669442f0f282/download/420938113-1629952094/mvtec_anomaly_detection.tar.xz
 tar -xf mvtec_anomaly_detection.tar.xz
-mv ../data/MVTec-AD/mvtec_anomaly_detection
+mv mvtec ../data/MVTec-AD/mvtec_anomaly_detection
 rm mvtec_anomaly_detection.tar.xz
--- experiments/train_torch.sh
+++ experiments/train_torch.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=../:$PYTHONPATH
-CUDA_VISIBLE_DEVICES=$3 python -m torch.distributed.launch --master_port $4 --nproc_per_node=$2 ../tools/train_val.py --config $1
+CUDA_VISIBLE_DEVICES=$3 python -m torch.distributed.launch --use_env --master_port $4 --nproc_per_node=$2 ../tools/train_val.py --config $1
